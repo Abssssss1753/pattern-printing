b103bdec0c6654f2f2c3ebc34c54398de6b16b0c
Abssssss1753 <77192440+Abssssss1753@users.noreply.github.com>
2021-01-27 10:27:40 +0530

    Add files via upload

---
+++ Ex 4.py
@@ -0,0 +1,45 @@
+################### pattern printing #############################
+# print("Enter the number you want to rows")
+# num=int(input())
+# print("enter 0 and 1")
+# print("Enter 0 for false and 1 for true")
+# b=input()
+#
+# if b=="1":
+#     for i in range(0,num+1):
+#         print("*"* i)
+#
+# elif b=="0":
+#     for i in range(num,0,-1):
+#         print("*"* i)
+
+##################################################### pattern printing A ########################
+
+# for row in range(7):
+#     for col in range(5):
+#         if row==0 and col in {1,2,3}:
+#             print("*",end="")
+#         elif row in {1,2,4,5,6} and col in {0,4}:
+#             print("*",end="")
+#         elif row==3:
+#             print("*",end="")
+#         else:
+#             print(" ",end="")
+#     print()
+
+############################## B ####################
+# for row in range(7):
+#     for col in range(5):
+#         if row in {0,6} and col in {0,3}:
+#             print("*",end="")
+#         elif row in {1,2,4,5} and col in {0,4}:
+#             print("*",end="")
+#         elif row==3 and col in {0,3}:
+#             print("*",end="")
+#         else:
+#             print(" ",end="")
+#     print()
+
+
+
+
